f792ecdcdd86f52751686d5185e30a1b00fd8131
yamayuz <yuki@yamayulab.com>
2022-02-11 23:24:14 +0900

    【update】カロリーをおにぎり換算で表示する機能を追加

--- calConversionApp/urls.py
+++ calConversionApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ConvView, TestView
+from .views import ConvView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('conv/', ConvView.as_view(), name='conv'),
-]
+] 
--- calConversionApp/views.py
+++ calConversionApp/views.py
@@ -2,22 +2,28 @@ from django.shortcuts import render
 from django.views.generic import View
 
 MECHANICAL_EQUIVALENT_OF_HEAT = 4.184
-
+SI_PREFIXES = 1000
 
 class ConvView(View):
     def get(self, request):
         context = {
             'cal': '0',
             'joule': '0',
+            'onigiri': '0',
+            'gasoline': '0',
         }
         return render(request, 'conversion.html', context)
 
     def post(self, request):
         cal = float(request.POST['cal'])
-        joule = cal * MECHANICAL_EQUIVALENT_OF_HEAT
+        joule = round(cal * MECHANICAL_EQUIVALENT_OF_HEAT, 1)
+        num_onigiri = round(cal / 180, 1)
+        num_gasoline = round(joule / 33360, 1)
         context = {
             'cal': cal,
             'joule': joule,
+            'onigiri': num_onigiri,
+            'gasoline': num_gasoline,
         }
         return render(request, 'conversion.html', context)
 
